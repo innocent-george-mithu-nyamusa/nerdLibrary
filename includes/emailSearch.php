add9ea19233719d7904b9078ff9185195dd63176
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-06-20 13:54:36 +0200

    removed search values

--- includes/emailSearch.php
+++ includes/emailSearch.php
@@ -8,8 +8,6 @@ include "../vendor/autoload.php";
 
 if(isset($_POST["email"])) {
 
-    echo "enetered email chheck";
-
     $userObj = new UserView();
     $emailExists = $userObj->checkEmail($_POST["email"]);
     if($emailExists){
